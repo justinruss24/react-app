42af016ec57b0ad819584d26c6f6c5c56b9d1766
Justin Russell <Justin@Justins-MacBook.local>
2020-05-27 18:21:18 -0700

    added high temp

--- app/src/components/timeStamp.js
+++ app/src/components/timeStamp.js
@@ -4,19 +4,19 @@ import styled from 'styled-components';
 const TimeDiv = styled.div `
     margin-bottom: 2%;
     color: white;
-    font-size: 2rem;
+    font-size: 3rem;
     margin-top: -10%;
 `
 
 const today = new Date();
 const time = today.getHours() + ":" + today.getMinutes();
+console.log(Date.now());
 
 const Time = () => {
     return (
         <TimeDiv>
             <h1>{time}</h1>
         </TimeDiv>
-        
     )
 }
 
--- app/src/components/weatherCard.js
+++ app/src/components/weatherCard.js
@@ -10,10 +10,12 @@ const Weather = props => {
         return Math.floor((num - 273.15) * 9/5 + 32);
     }
     const temp = props.weather.temp;
+    const highTemp = props.weather.temp_max;
 
     console.log(props);
 
     const decider = converter(temp);
+    const high = converter(highTemp);
 
     function canGolf(decider) {
        if ( isNaN(decider) ) {
@@ -24,6 +26,7 @@ const Weather = props => {
              <h3 id="temp1" className="temp">
                {decider}°F?!
              </h3>
+             <h3 className="temp">(Temp High: {high}°F)</h3>
              <h4>You bet your sweet ass he is.</h4>
            </div>
          );
